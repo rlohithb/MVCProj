facebc76f14babe4d15b38033ddbabae4fe8dd6f
LB <rlohithb@gmail.com>
2021-02-11 00:19:40 -0500

    First MVC project

---
+++ MyFirstMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyFirstMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        /*
+         * public IActionResult Index()
+        {
+            return View();
+        }
+        */
+        public string Index()
+        {
+            return "This is my default action.....";
+        }
+        
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+        }
+    }
+}
